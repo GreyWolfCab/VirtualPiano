f21cd4e492070fb1930ca68af0ffe5fefd8cf992
GreyWolfCab <54644740+GreyWolfCab@users.noreply.github.com>
2020-12-28 17:31:11 -0800

    Key highlighting, plus cleanup.

--- main.js
+++ main.js
@@ -1,82 +1,160 @@
 class PianoKey {
-    constructor(key, note) {
-        this.key = key;
-        this.note = note;
+    //constructor for each key object
+    constructor(key, isWhite, note, documentElement) {
+        this.key = key;//which key is represented
+        this.isWhite = isWhite;//type of key is white or black
+        this.note = note;//audio object associated with key
+        this.element = documentElement;//DOM element associated with key
     }
 
+    //display to the user this key was selected
+    select() {
+        this.element.style.backgroundColor = "green";
+    }
+
+    //display to the user this key is no longer selected
+    deselect() {
+        if (this.isWhite) {
+            this.element.style.backgroundColor = "white"
+        } else if (!this.isWhite) {
+            this.element.style.backgroundColor = "black";
+        }
+    }
+
+    //play the audio file associated with the key
     play() {
         this.note.play();
     }
 
+    //stop the audio file
     pause() {
         this.note.pause();
-        this.note.currentTime = 0;
+        this.note.currentTime = 0;//allow for the audio to be restarted immediately
     }
 
+    //interrupt the note if it's already playing and then restart the audio
     press() {
         this.pause();
         this.play();
     }
 }
 
+//create variables for each playable key
 //black-keys
-let wKey = new PianoKey("W", new Audio("PianoNotes/W.mp3"));
-let eKey = new PianoKey("E", new Audio("PianoNotes/E.mp3"));
-let tKey = new PianoKey("T", new Audio("PianoNotes/T.mp3"));
-let yKey = new PianoKey("Y", new Audio("PianoNotes/Y.mp3"));
-let uKey = new PianoKey("U", new Audio("PianoNotes/U.mp3"));
+let wKey = new PianoKey("W", false, new Audio("PianoNotes/W.mp3"), document.getElementById("W"));
+let eKey = new PianoKey("E", false, new Audio("PianoNotes/E.mp3"), document.getElementById("E"));
+let tKey = new PianoKey("T", false, new Audio("PianoNotes/T.mp3"), document.getElementById("T"));
+let yKey = new PianoKey("Y", false, new Audio("PianoNotes/Y.mp3"), document.getElementById("Y"));
+let uKey = new PianoKey("U", false, new Audio("PianoNotes/U.mp3"), document.getElementById("U"));
 
 //white-keys
-let aKey = new PianoKey("A", new Audio("PianoNotes/A.mp3"));
-let sKey = new PianoKey("S", new Audio("PianoNotes/S.mp3"));
-let dKey = new PianoKey("D", new Audio("PianoNotes/D.mp3"));
-let fKey = new PianoKey("F", new Audio("PianoNotes/F.mp3"));
-let gKey = new PianoKey("G", new Audio("PianoNotes/G.mp3"));
-let hKey = new PianoKey("H", new Audio("PianoNotes/H.mp3"));
-let jKey = new PianoKey("J", new Audio("PianoNotes/J.mp3"));
+let aKey = new PianoKey("A", true, new Audio("PianoNotes/A.mp3"), document.getElementById("A"));
+let sKey = new PianoKey("S", true, new Audio("PianoNotes/S.mp3"), document.getElementById("S"));
+let dKey = new PianoKey("D", true, new Audio("PianoNotes/D.mp3"), document.getElementById("D"));
+let fKey = new PianoKey("F", true, new Audio("PianoNotes/F.mp3"), document.getElementById("F"));
+let gKey = new PianoKey("G", true, new Audio("PianoNotes/G.mp3"), document.getElementById("G"));
+let hKey = new PianoKey("H", true, new Audio("PianoNotes/H.mp3"), document.getElementById("H"));
+let jKey = new PianoKey("J", true, new Audio("PianoNotes/J.mp3"), document.getElementById("J"));
 
+//play corresponding key when the user presses a defined key
 document.addEventListener("keypress", function(event) {
+    let currentKey = null;
     switch (event.code) {
         //black keys
         case "KeyW":
-            wKey.press();
+            currentKey = wKey;
             break;
         case "KeyE":
-            eKey.press();
+            currentKey = eKey;
             break;
         case "KeyT":
-            tKey.press();
+            currentKey = tKey;
             break;
         case "KeyY":
-            yKey.press();
+            currentKey = yKey;
             break;
         case "KeyU":
-            uKey.press();
+            currentKey = uKey;
             break;
         //white keys
         case "KeyA":
-            aKey.press();
+            currentKey = aKey;
             break;
         case "KeyS":
-            sKey.press();
+            currentKey = sKey;
             break;
         case "KeyD":
-            dKey.press();
+            currentKey = dKey;
             break;        
         case "KeyF":
-            fKey.press();
+            currentKey = fKey;
             break;
         case "KeyG":
-            gKey.press();
+            currentKey = gKey;
             break;
         case "KeyH":
-            hKey.press();
+            currentKey = hKey;
             break;
         case "KeyJ":
-            jKey.press();
+            currentKey = jKey;
             break;
         default:
-            console.log("Undefined Key");
+            console.log("Undefined Key Press");
             break;
+    }
+    if (currentKey != null) {//perform function if the key is defined
+        currentKey.select();
+        currentKey.press();
+    }
+});
+
+//execute function when the corresponding key is no longer pressed
+document.addEventListener("keyup", function(event) {
+    let currentKey = null;
+    switch (event.code) {
+        //black keys
+        case "KeyW":
+            currentKey = wKey;
+            break;
+        case "KeyE":
+            currentKey = eKey;
+            break;
+        case "KeyT":
+            currentKey = tKey;
+            break;
+        case "KeyY":
+            currentKey = yKey;
+            break;
+        case "KeyU":
+            currentKey = uKey;
+            break;
+        //white keys
+        case "KeyA":
+            currentKey = aKey;
+            break;
+        case "KeyS":
+            currentKey = sKey;
+            break;
+        case "KeyD":
+            currentKey = dKey;
+            break;        
+        case "KeyF":
+            currentKey = fKey;
+            break;
+        case "KeyG":
+            currentKey = gKey;
+            break;
+        case "KeyH":
+            currentKey = hKey;
+            break;
+        case "KeyJ":
+            currentKey = jKey;
+            break;
+        default:
+            console.log("Undefined Key Up");
+            break;
+    }
+    if (currentKey != null) {//perform function if the key is defined
+        currentKey.deselect();
     }
 });
